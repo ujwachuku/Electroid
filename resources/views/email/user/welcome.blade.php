2f39bc9a0694656b2383997600c954638d9bc207
Cor Cronje <cronje.cor@gmail.com>
2019-10-01 11:06:23 +0200

    Updates to mailables

--- resources/views/email/user/welcome.blade.php
+++ resources/views/email/user/welcome.blade.php
@@ -3,7 +3,7 @@
 
 Hi there {{ $user->name }}, your new account is ready, you should receive another email with a link to set your password, or you can click the button below to reset the password yourself.
 
-@component('mail::button', ['url' => 'localhost:8000/password/reset?email=' . $user->email])
+@component('mail::button', ['url' => config('APP_URL') . '/password/reset?email=' . $user->email])
 Reset My Password
 @endcomponent
 
